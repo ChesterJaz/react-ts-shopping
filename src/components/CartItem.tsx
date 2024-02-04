c7b12adab43d8eaf2a41ad96dd40cadc3f94224d
Chester <89991461+ChesterJaz@users.noreply.github.com>
2024-02-05 00:10:54 +0800

    1

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCart";
 import cartItems from "../data/items.json";
+import { CurrencyFormat } from "../utils/CurrencyFormat";
 
 type cartItemsProps = {
     id: number;
@@ -20,7 +21,16 @@ export const CartItem = ({id, qty} : cartItemsProps) => {
             <div>
                 {items.name} {qty > 1 && <span className="text-muted" style={{fontSize: '.80rem'}}>x{qty}</span>}
             </div>
+            <div className="text-muted" style={{fontSize: '.80rem'}}>
+                {CurrencyFormat(items.price)}
+            </div>
         </div>
+
+        <div>
+            {CurrencyFormat(items.price * qty)}
+        </div>
+        <Button variant="outline-danger" size="sm" onClick={() => remove(items.id)}>x</Button>
+
    </Stack>
   )
 }
